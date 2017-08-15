a322a6e053cd80b7b43ebc88512deca0920865ae
Garth Gaughan <garth0323@gmail.com>
2017-08-14 17:32:36 -0700

    first response

--- app/models/service.rb
+++ app/models/service.rb
@@ -5,6 +5,12 @@ class Service
     Savon.client(wsdl: 'https://rxnav.nlm.nih.gov/RxNormDBService.xml')
   end
 
-  
+  def get_drugs(drug)
+    message = {name: drug}
+    response = client.call(:get_drugs, message: message)
+    response.body
+  end
+
+
 
 end
