951f76ba9980091b5e23e9827c0a544a589054c9
Garth Gaughan <garth0323@gmail.com>
2017-08-14 19:55:29 -0700

    more responses

--- app/models/service.rb
+++ app/models/service.rb
@@ -11,6 +11,15 @@ class Service
     response.body
   end
 
+  def find_cui(drug)
+    message = {term: drug}
+    response = client.call(:find_rxcui_by_string, message: message)
+    response.body
+  end
 
-
+  def spelling_suggestions(drug)
+    message = {term: drug}
+    response = client.call(:get_spelling_suggestions, message: message)
+    response.body
+  end
 end
